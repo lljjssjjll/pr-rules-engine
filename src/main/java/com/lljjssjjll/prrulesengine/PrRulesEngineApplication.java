a5152894f890b195a357f5de71a31d43c45ed025
lljjssjjll <lljjssjjll@gmail.com>
2021-12-09 17:40:00 +0900

    Easy Rules 디펜던시 추가

---
+++ src/main/java/com/lljjssjjll/prrulesengine/PrRulesEngineApplication.java
@@ -0,0 +1,13 @@
+package com.lljjssjjll.prrulesengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrRulesEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrRulesEngineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lljjssjjll/prrulesengine/PrRulesEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lljjssjjll.prrulesengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrRulesEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
